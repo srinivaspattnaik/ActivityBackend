1e45f4c01240da98ee57d0bc79c370c90ac43f01
abbas886 <abbas886@yahoo.com>
2017-09-07 17:31:02 +0530

    update method signatures

--- src/main/java/com/stackroute/activitystream/dao/CircleDAO.java
+++ src/main/java/com/stackroute/activitystream/dao/CircleDAO.java
@@ -5,8 +5,12 @@ import java.util.List;
 public interface CircleDAO 
 {
 	public boolean createCircle(Circle circle);
+	//This method should take two parameters circleID and emailid(owner)
+	//Only owner can delete the circle.
 	public boolean removeCircle(String circleID);
 	public boolean updateCircle(Circle circle);
+	
+	//The return type can be List<String> i.e., need only circle ids.
 	public List<Circle> getMyCircles(String emailid);
 	public List<String> getAllCircleID();
 }
